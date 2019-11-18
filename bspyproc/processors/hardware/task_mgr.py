2ad2ff5d8aa60e7929c77903a0298819961a1beb
unai <u.alegre@utwente.nl>
2019-11-18 12:52:42 +0100

    Minor fix. os to time for sleeping.

--- bspyproc/processors/hardware/task_mgr.py
+++ bspyproc/processors/hardware/task_mgr.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 DEFAULT_IP = '192.168.1.5'
 DEFAULT_SUBNET_MASK = '255.255.255.0'
@@ -23,9 +24,9 @@ def run_server(ip=DEFAULT_IP, subnet_mask=DEFAULT_SUBNET_MASK, port=DEFAULT_PORT
 def set_static_ip(ip=DEFAULT_IP, subnet_mask=DEFAULT_SUBNET_MASK):
     SET_STATIC_IP_COMMAND = f"ifconfig eth0 {ip} netmask {subnet_mask} up"
     os.system(SET_STATIC_IP_COMMAND)
-    os.sleep(1)
+    time.sleep(1)
     os.system(SWITCH_ETHERNET_OFF_COMMAND)
-    os.sleep(1)
+    time.sleep(1)
     os.system(SWITCH_ETHERNET_ON_COMMAND)
 
 
