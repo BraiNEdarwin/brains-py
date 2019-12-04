e3a527bed512509a19d4b22f80d078ba24013566
ualegre <u.alegre@utwente.nl>
2019-12-04 17:47:21 +0100

    Working processor architecture for 21 and 221 with batch normalisation.

--- bspyproc/architectures/multiplexing/processor.py
+++ bspyproc/architectures/multiplexing/processor.py
@@ -1,97 +1,126 @@
 
+import torch
+import torch.nn as nn
+import numpy as np
 from bspyproc.processors.processor_mgr import get_processor
+from bspyproc.utils.pytorch import TorchUtils
 
 
-class TwoToOneProcessor():
+class ArchitectureProcessor():
     def __init__(self, configs):
         self.configs = configs
         self.processor = get_processor(configs)
+        self.clipping_value = 3.55 * self.get_amplification_value()
+        self.conversion_offset = -0.6
 
-    def get_output(self, x):
-        # Pass through input layer
-        # x = (self.scale * x) + self.offset
-        # configs "input_indices": [0, 1]
-        x1 = self.processor.get_output(x[:, 0:7])
-        x2 = self.processor.get_output(x[:, 7:14])
-        # [ i1, i2, c1 , c2, c3, ...  ]
-        # x [0:21]
-        # x[0:7,:] = [ i1, i2, c1, c2, c3, c4, c5]
-        # x[7:14] =  [i1, i2, c1, c2, c3, c4, c5]
-        # x[14:21] =  [x1, x2, c1, c2, c3, c4, c5]
-        # [i1, i2, c1, c2, c3, c4, c5]
-        # --- BatchNorm --- #
-        # h = self.bn1(torch.cat((x1, x2), dim=1))
-        # std1 = np.sqrt(torch.mean(self.bn1.running_var).cpu().numpy())
-        # cut = 2 * std1
+    def clip(self, x):
+        x[x > self.clipping_value] = self.clipping_value
+        return x
+
+    def batch_norm(self, bn, x1, x2):
+        x1 = TorchUtils.get_tensor_from_numpy(x1)
+        x2 = TorchUtils.get_tensor_from_numpy(x2)
+        h = bn(torch.cat((x1, x2), dim=1))
+        std1 = np.sqrt(torch.mean(bn.running_var).cpu().numpy())
+        cut = 2 * std1
         # Pass it through output layer
-        # h = torch.tensor(1.8 / (4 * std1)) * \
-        #    torch.clamp(h, min=-cut, max=cut) + self.conversion_offset
-
-        x[:, 14] = x1[:, 0]
-        x[:, 15] = x2[:, 0]
-        result = self.processor.get_output(x[:, 14:])
-        # --- BatchNorm --- #
-        # h = self.bn1(torch.cat((x1, x2), dim=1))
-        # std1 = np.sqrt(torch.mean(self.bn1.running_var).cpu().numpy())
-        # cut = 2 * std1
-        # Pass it through output layer
-        # h = torch.tensor(1.8 / (4 * std1)) * \
-        #    torch.clamp(h, min=-cut, max=cut) + self.conversion_offset
-
-        return result
+        h = torch.tensor(1.8 / (4 * std1)) * \
+            torch.clamp(h, min=-cut, max=cut) + self.conversion_offset
+        return h.numpy()
 
     def get_amplification_value(self):
         return self.processor.get_amplification_value()
 
 
-class TwoToTwoToOneProcessor():
+class TwoToOneProcessor(ArchitectureProcessor):
     def __init__(self, configs):
-        self.configs = configs
-        self.processor = get_processor(configs)
+        super().__init__(configs)
+        if configs['batch_norm']:
+            self.bn1 = nn.BatchNorm1d(2, affine=False)
+            self.process_layer1 = self.process_layer1_batch_norm
+        else:
+            self.process_layer1 = self.process_layer1_alone
 
     def get_output(self, x):
-        # Pass through input layer
-        # x = (self.scale * x) + self.offset
-        # configs "input_indices": [0, 1]
         x1 = self.processor.get_output(x[:, 0:7])
         x2 = self.processor.get_output(x[:, 7:14])
 
-        # --- BatchNorm --- #
-        # h = self.bn1(torch.cat((x1, x2), dim=1))
-        # std1 = np.sqrt(torch.mean(self.bn1.running_var).cpu().numpy())
-        # cut = 2 * std1
-        # Pass it through output layer
-        # h = torch.tensor(1.8 / (4 * std1)) * \
-        #    torch.clamp(h, min=-cut, max=cut) + self.conversion_offset
+        x = self.process_layer1(x, x1, x2)
+        result = self.processor.get_output(x[:, 14:])
 
-        x[:, 14] = x1[:, 0]
-        x[:, 15] = x2[:, 0]
-        x[:, 21] = x1[:, 0]
-        x[:, 22] = x2[:, 0]
+        return self.process_output_layer(result)
+
+    def process_layer1_alone(self, x, x1, x2):
+        x[:, 14] = self.clip(x1[:, 0])
+        x[:, 15] = self.clip(x2[:, 0])
+        return x
+
+    def process_layer1_batch_norm(self, x, x1, x2):
+        bnx = self.batch_norm(self.bn1, x1, x2)
+
+        x[:, 14] = self.clip(bnx[:, 0])
+        x[:, 15] = self.clip(bnx[:, 1])
+        return x
+
+    def process_output_layer(self, y):
+        return self.clip(y)
+
+
+class TwoToTwoToOneProcessor(ArchitectureProcessor):
+    def __init__(self, configs):
+        super().__init__(configs)
+        if configs['batch_norm']:
+            self.bn1 = nn.BatchNorm1d(2, affine=False)
+            self.bn2 = nn.BatchNorm1d(2, affine=False)
+            self.process_layer1 = self.process_layer1_batch_norm
+            self.process_layer2 = self.process_layer2_batch_norm
+        else:
+            self.process_layer1 = self.process_layer1_alone
+            self.process_layer2 = self.process_layer2_alone
+
+    def get_output(self, x):
+
+        x1 = self.processor.get_output(x[:, 0:7])
+        x2 = self.processor.get_output(x[:, 7:14])
+
+        x = self.process_layer1(x, x1, x2)
 
         h1 = self.processor.get_output(x[:, 14:21])
         h2 = self.processor.get_output(x[:, 21:28])
-        # --- BatchNorm --- #
-        # h = self.bn1(torch.cat((x1, x2), dim=1))
-        # std1 = np.sqrt(torch.mean(self.bn1.running_var).cpu().numpy())
-        # cut = 2 * std1
-        # Pass it through output layer
-        # h = torch.tensor(1.8 / (4 * std1)) * \
-        #    torch.clamp(h, min=-cut, max=cut) + self.conversion_offset
 
-        x[:, 28] = h1[:, 0]
-        x[:, 29] = h2[:, 0]
+        x = self.process_layer2(x, h1, h2)
+
         result = self.processor.get_output(x[:, 28:])
 
-        # --- BatchNorm --- #
-        # h = self.bn1(torch.cat((x1, x2), dim=1))
-        # std1 = np.sqrt(torch.mean(self.bn1.running_var).cpu().numpy())
-        # cut = 2 * std1
-        # Pass it through output layer
-        # h = torch.tensor(1.8 / (4 * std1)) * \
-        #    torch.clamp(h, min=-cut, max=cut) + self.conversion_offset
+        return self.process_output_layer(result)
 
-        return result
+    def process_layer1_alone(self, x, x1, x2):
+        x[:, 14] = self.clip(x1[:, 0])
+        x[:, 15] = self.clip(x2[:, 0])
+        x[:, 21] = self.clip(x1[:, 0])
+        x[:, 22] = self.clip(x2[:, 0])
+        return x
 
-    def get_amplification_value(self):
-        return self.processor.get_amplification_value()
+    def process_layer1_batch_norm(self, x, x1, x2):
+        bnx = self.batch_norm(self.bn1, x1, x2)
+
+        x[:, 14] = self.clip(bnx[:, 0])
+        x[:, 15] = self.clip(bnx[:, 1])
+        x[:, 21] = self.clip(bnx[:, 0])
+        x[:, 22] = self.clip(bnx[:, 1])
+        return x
+
+    def process_layer2_alone(self, x, x1, x2):
+        x[:, 28] = self.clip(x1[:, 0])
+        x[:, 29] = self.clip(x2[:, 0])
+        return x
+
+    def process_layer2_batch_norm(self, x, x1, x2):
+        bnx = self.batch_norm(self.bn2, x1, x2)
+
+        x[:, 28] = self.clip(bnx[:, 0])
+        x[:, 29] = self.clip(bnx[:, 0])
+        return x
+
+    def process_output_layer(self, y):
+        return self.clip(y)
