ef0c9276896169fc8c9e1f5e26f790f915ea6300
ualegre <u.alegre@utwente.nl>
2020-01-21 17:34:12 +0100

    Added normalisation method

--- bspyproc/utils/input.py
+++ bspyproc/utils/input.py
@@ -2,7 +2,7 @@ import numpy as np
 from scipy import signal
 
 
-def generate_triangle(self, freq, t, amplitude, fs, phase=np.zeros(7)):
+def generate_triangle(freq, t, amplitude, fs, phase=np.zeros(7)):
     '''
     Generates a triangle wave form that can be used for the input data.
     freq:       Frequencies of the inputs in an one-dimensional array
@@ -16,7 +16,7 @@ def generate_triangle(self, freq, t, amplitude, fs, phase=np.zeros(7)):
     return signal.sawtooth((2 * np.pi * freq[:, np.newaxis] * t) / fs + phase[:, np.newaxis] + np.pi / 2, 0.5) * amplitude[:, np.newaxis]
 
 
-def generate_sinewave(self, freq, t, amplitude, fs, phase=np.zeros(7)):
+def generate_sinewave(freq, t, amplitude, fs, phase=np.zeros(7)):
     '''
     Generates a sine wave that can be used for the input data.
     freq:       Frequencies of the inputs in an one-dimensional array
@@ -27,3 +27,7 @@ def generate_sinewave(self, freq, t, amplitude, fs, phase=np.zeros(7)):
     '''
 
     return np.sin((2 * np.pi * freq[:, np.newaxis] * t) / fs + phase[:, np.newaxis]) * amplitude[:, np.newaxis]
+
+
+def normalise(x, eps=1e-5):
+    return ((x - np.mean(x)) / (np.sqrt(np.var(x) + eps)))
