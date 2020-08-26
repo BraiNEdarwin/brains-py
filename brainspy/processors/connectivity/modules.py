a6b84cbedb12c7438c68275cac3709c0752e6177
ualegre <u.alegre@utwente.nl>
2020-08-26 12:45:31 +0200

    Major improvements of the DNPUBN module. Current to voltage conversion is automated, and it happens before the DNPU layer. The DNPUBN module has been moved to modules.py

--- brainspy/processors/connectivity/modules.py
+++ brainspy/processors/connectivity/modules.py
@@ -10,9 +10,10 @@ import torch
 import numpy as np
 import torch.nn as nn
 import torch.nn.functional as nf
-from more_itertools import grouper
-from brainspy.processors.simulation.network import TorchModel
+
+from brainspy.processors.dnpu import DNPU
 from brainspy.utils.pytorch import TorchUtils
+from brainspy.utils.transforms import CurrentToVoltage
 
 
 class DNPU_Base(nn.Module):
@@ -148,13 +149,49 @@ class Local_Receptive_Field(DNPU_Base):
         return x.view(-1, self.out_size, self.out_size)
 
 
+class DNPUBN(nn.Module):
+    '''
+        v_min value is the minimum voltage value of the electrode of the next dnpu to which the output is going to be connected
+        v_max value is the maximum voltage value of the electrode of the next dnpu to which the output is going to be connected 
+    '''
+
+    def __init__(self, configs, current_range=torch.tensor([[-2, 2], [-2, 2]]), current_to_voltage=True, batch_norm=True):
+        # default current_range = 2  * std, where std is assumed to be 1
+        super().__init__()
+        self.dnpu = DNPU(configs)
+        if batch_norm:
+            self.bn = TorchUtils.format_tensor(nn.BatchNorm1d(1, affine=False))
+        else:
+            self.bn = batch_norm
+        if current_to_voltage:
+            self.current_to_voltage = CurrentToVoltage(current_range, self.dnpu.get_input_ranges())
+        else:
+            self.current_to_voltage = current_to_voltage
+
+    def forward(self, x):
+        if self.current_to_voltage:
+            x = self.current_to_voltage(x)
+        x = self.dnpu(x)
+        # Cut off values out of the clipping value
+        x = torch.clamp(x, min=self.dnpu.processor.clipping_value[0], max=self.dnpu.processor.clipping_value[1])
+        # Apply batch normalisation
+        if self.bn:
+            x = self.bn(x)
+        # Apply current to voltage transformation
+        # x = self.current_to_voltage(x)
+        return x
+
+    def hw_eval(self, hw_processor_configs):
+        self.dnpu.hw_eval(hw_processor_configs)
+
+
 if __name__ == '__main__':
     from brainspy.utils.io import load_configs
     import matplotlib.pyplot as plt
     import time
 
     NODE_CONFIGS = load_configs('/home/hruiz/Documents/PROJECTS/DARWIN/Code/packages/brainspy/brainspy-processors/configs/configs_nn_model.json')
-    node = TorchModel(NODE_CONFIGS)
+    node = DNPU(NODE_CONFIGS)
     # linear_layer = nn.Linear(20, 3).to(device=TorchUtils.get_accelerator_type())
     # dnpu_layer = DNPU_Channels([[0, 3, 4]] * 1000, node)
     linear_layer = nn.Linear(20, 300).to(device=TorchUtils.get_accelerator_type())
--- brainspy/utils/transforms.py
+++ brainspy/utils/transforms.py
@@ -6,17 +6,36 @@ from brainspy.utils.waveform import WaveformManager
 from brainspy.utils.electrodes import get_map_to_voltage_vars
 
 
+# class CurrentToVoltage():
+#     def __init__(self, v_min, v_max, x_min=-1, x_max=1, cut=True):
+#         self.scale, self.offset = get_map_to_voltage_vars(v_min, v_max, x_min, x_max)
+#         self.x_min = x_min
+#         self.x_max = x_max
+#         self.cut = cut
+
+#     def __call__(self, x):
+#         if self.cut:
+#             x = torch.clamp(x, min=self.x_min, max=self.x_max)
+#         x = (x * self.scale) + self.offset
+#         return x
+
 class CurrentToVoltage():
-    def __init__(self, v_min, v_max, x_min=-1, x_max=1, cut=True):
-        self.scale, self.offset = get_map_to_voltage_vars(v_min, v_max, x_min, x_max)
-        self.x_min = x_min
-        self.x_max = x_max
+    def __init__(self, current_range, voltage_range, cut=True):
+        assert len(current_range) == len(voltage_range), 'Mapping ranges are different in length'
+        self.map_variables = TorchUtils.get_tensor_from_list([get_map_to_voltage_vars(voltage_range[i][0], voltage_range[i][1],
+                                                                                      current_range[i][0], current_range[i][1]) for i in range(len(current_range))])
+        self.current_range = current_range
         self.cut = cut
 
     def __call__(self, x):
-        if self.cut:
-            x = torch.clamp(x, min=self.x_min, max=self.x_max)
-        x = (x * self.scale) + self.offset
+        assert len(x.shape) == 2 and x.shape[1] == len(self.map_variables), 'Input shape not supported.'
+        for i in range(len(self.map_variables)):
+            # Linear transformation variables are as follows
+            # SCALE: self.map_variables[i][0]
+            # OFFSET: self.map_variables[i][1]
+            if self.cut:
+                x[:, i] = torch.clamp(x[:, i], min=self.current_range[i][0], max=self.current_range[i][1])
+            x[:, i] = (x[:, i] * self.map_variables[i][0]) + self.map_variables[i][1]
         return x
 
 
