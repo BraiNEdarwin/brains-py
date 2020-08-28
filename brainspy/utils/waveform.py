1b1003da40854e1645e19fab0304430fa2b52525
ualegre <u.alegre@utwente.nl>
2020-08-28 16:53:18 +0200

    Major improvements on the waveform generation. Tests included

--- brainspy/utils/waveform.py
+++ brainspy/utils/waveform.py
@@ -3,7 +3,8 @@
     received by the hardware DNPUs (Dopant Network Processing Units).
 """
 import torch
-import numpy as np
+
+# import numpy as np
 
 
 class WaveformManager:
@@ -32,8 +33,17 @@ class WaveformManager:
     """
 
     def __init__(self, configs):
-        self.plateau_lengths = configs["plateau_lengths"]
-        self.slope_lengths = configs["slope_lengths"]
+        self.plateau_length = configs["plateau_lengths"]
+        self.slope_length = configs["slope_lengths"]
+        self.generate_mask_base()
+
+    def generate_mask_base(self):
+        mask = []
+        final_mask = [False] * self.slope_length
+        mask += final_mask
+        mask += [True] * self.plateau_length
+        self.initial_mask = torch.tensor(mask)
+        self.final_mask = torch.tensor(final_mask)
 
     def _expand(self, parameter, length):
         """The aim of this function is to format the amplitudes and
@@ -48,37 +58,33 @@ class WaveformManager:
         """
         Generates a waveform (voltage input over time) with constant intervals of value amplitudes[i] for interval i of length[i].
 
-        amplitudes = The input from which a waveform will be generated. The input is in form of a list.
-        plateau_lengths = The number of points used to represent the amplitudes. It can be provided as a single
+        plateaus = The input from which a waveform will be generated. The input is in form of a list.
+        plateau_length = The number of points used to represent the amplitudes. It can be provided as a single
         number or as a list in which all the length values will correspond to its corresponding amplitude value.
         slope_lengths = The number of points of the slope.
 
         The output is in list format
         """
-        output = np.ndarray([])
+        data_size = len(data) - 1
+        # output = torch.tensor([])
         # data = list(self.safety_format(data, safety_formatting))
-        plateau_lengths = self._expand(self.plateau_lengths, len(data))
-        slope_lengths = self._expand(self.slope_lengths, len(data))
+        # plateau_lengths = self._expand(self.plateau_lengths, len(data))
+        # slope_lengths = self._expand(self.slope_lengths, len(data))
         # amplitudes, plateau_lengths, slope_lengths = self.format_amplitudes_and_slopes(amplitudes, self.plateau_lengths, self.slope_lengths)
 
-        if len(data) == len(plateau_lengths) == len(slope_lengths):
-            output = np.linspace(0, data[0], slope_lengths[0])
-            data_size = len(data) - 1
-            for i in range(data_size):
-                output = np.concatenate(
-                    (output, np.array(([data[i]] * plateau_lengths[i])))
-                )
-                output = np.concatenate(
-                    (output, np.linspace(data[i], data[i + 1], slope_lengths[i]))
-                )
-            i = data_size
-            output = np.concatenate(
-                (output, np.array(([data[i]] * plateau_lengths[i])))
+        # if len(data) == len(plateau_lengths) == len(slope_lengths):
+        output = torch.linspace(0, data[0], self.slope_length)
+        for i in range(data_size):
+            output = torch.cat((output, data[i].repeat(self.plateau_length)))
+            output = torch.cat(
+                (output, torch.linspace(data[i], data[i + 1], self.slope_length))
             )
-            output = np.concatenate((output, np.linspace(data[i], 0, slope_lengths[i])))
+        i = data_size
+        output = torch.cat((output, data[i].repeat(self.plateau_length)))
+        output = torch.cat((output, torch.linspace(data[i], 0, self.slope_length)))
 
-        else:
-            assert False, "Assignment of amplitudes and lengths/slopes is not unique!"
+        # else:
+        #     assert False, "Assignment of amplitudes and lengths/slopes is not unique!"
         return output
 
     # def points_to_plateau(self, data):
@@ -91,14 +97,14 @@ class WaveformManager:
 
     def points_to_plateaus(self, data):
         # output = np.ndarray([])
-        result = data[0].repeat(self.plateau_lengths, 1)
-        for i in range(1, len(data)):
-            result = torch.cat((result, data[i].repeat(self.plateau_lengths, 1)), dim=0)
+        # result = data[0].repeat(self.plateau_length, 1)
+        # for i in range(1, len(data)):
+        #     result = torch.cat((result, data[i].repeat(self.plateau_length, 1)), dim=0)
         # plateau_lengths = self._expand(self.plateau_lengths, len(data))
         # output = data[0].expand(data.shape[0] * plateau_lengths[0], -1)
         # for i in range(1, data.shape[1]):
         #     output = torch.cat((output, data[i].expand(data.shape[0] * plateau_lengths[i], -1)))
-        return result
+        return data.repeat(self.plateau_length, 1).T.flatten()
 
     def plateaus_to_waveform(self, data):
         """
@@ -110,56 +116,60 @@ class WaveformManager:
 
         The output is in list format
         """
-        output = np.ndarray([])
-        point_length = int(len(data) / self.plateau_lengths)
-        plateau_lengths = self._expand(self.plateau_lengths, point_length)
-        slope_lengths = self._expand(self.slope_lengths, point_length)
+        # output = np.ndarray([])
+        point_length = int(len(data) / self.plateau_length)
+        # plateau_lengths = self._expand(self.plateau_length, point_length)
+        # slope_lengths = self._expand(self.slope_length, point_length)
         i = 0
         j = 0
         mask = []
-        output = np.linspace(0, data[0], slope_lengths[0])
-        mask += [False] * slope_lengths[0]
+        output = torch.linspace(0, data[0], self.slope_length)
+        mask += [False] * self.slope_length
         data_size = point_length - 1
         for i in range(data_size):
-            current_plateau = np.array(data[j: j + plateau_lengths[i]])
+            current_plateau = data[j : j + self.plateau_length]
             mask += [True] * len(current_plateau)
-            current_slope = np.linspace(
-                data[j + plateau_lengths[i] - 1],
-                data[j + plateau_lengths[i]],
-                slope_lengths[i],
+            current_slope = torch.linspace(
+                data[j + self.plateau_length - 1],
+                data[j + self.plateau_length],
+                self.slope_length,
             )
             mask += [False] * len(current_slope)
-            output = np.concatenate((output, current_plateau))
-            output = np.concatenate((output, current_slope))
-            j += plateau_lengths[i]
+            output = torch.cat((output, current_plateau))
+            output = torch.cat((output, current_slope))
+            j += self.plateau_length
         i = data_size
-        current_plateau = np.array(data[j: j + plateau_lengths[i]])
-        current_slope = np.linspace(
-            data[j + plateau_lengths[i] - 1], 0, slope_lengths[i]
+        current_plateau = data[j : j + self.plateau_length]
+        current_slope = torch.linspace(
+            data[j + self.plateau_length - 1], 0, self.slope_length
         )
         mask += [True] * len(current_plateau)
         mask += [False] * len(current_slope)
-        output = np.concatenate((output, current_plateau))
-        output = np.concatenate((output, current_slope))
+        output = torch.cat((output, current_plateau))
+        output = torch.cat((output, current_slope))
         return output, mask
 
     def plateaus_to_points(self, data):
-        plateau_lengths = self._expand(
-            self.plateau_lengths, int(len(data) / self.plateau_lengths)
-        )
-        output = np.array([])
-        i = 0
-        for amplitude_length in plateau_lengths:
-            output = np.append(output, data[i: i + amplitude_length].mean())
-            i += amplitude_length
-        return output
+        # plateau_lengths = self._expand(
+        #     self.plateau_lengths, int(len(data) / self.plateau_lengths)
+        # )
+        # output = np.array([])
+        # j = 0
+        # for i in range(len(data)):
+        #     output = np.append(output, data[j: j + self.plateau_length].mean())
+        #     j += self.plateau_length
+        point_no = int(len(data) / self.plateau_length)
+        return data.view(point_no, self.plateau_length).mean(dim=1)
 
-    def waveform_to_points(self, data):
-        data = data[self.generate_mask(len(data))]
-        return self.plateaus_to_points(data)
+    def waveform_to_points(self, data, mask=None):
+        if mask is None:
+            mask = self.generate_mask(len(data))
+        return self.plateaus_to_points(data[mask])
 
-    def waveform_to_plateaus(self, data):
-        return data[self.generate_mask(len(data))]
+    def waveform_to_plateaus(self, data, mask=None):
+        if mask is None:
+            mask = self.generate_mask(len(data))
+        return data[mask]
 
     def generate_mask(self, data_size):
         """
@@ -171,29 +181,42 @@ class WaveformManager:
 
         The output is in list format
         """
-        assert isinstance(self.slope_lengths, int) and isinstance(
-            self.plateau_lengths, int
-        ), "Generate mask operation is only supported by integer slope and amplitude lengths"
-        mask = []
+        # assert isinstance(self.slope_length, int) and isinstance(
+        #     self.plateau_length, int
+        # ), "Generate mask operation is only supported by integer slope and amplitude lengths"
+        # mask = []
         # plateau_lengths = self.expand(self.plateau_lengths, data_length)
         # slope_lengths = self.expand(self.slope_lengths, data_length)
         # if data_length == len(plateau_lengths) == len(slope_lengths):
-        i = 0
-        odd = True
-        while i < data_size:
-            if odd:
-                mask += [False] * self.slope_lengths
-                i += self.slope_lengths
-                odd = False
-            else:
-                mask += [True] * self.plateau_lengths
-                i += self.plateau_lengths
-                odd = True
-        return mask
+        # i = 0
+        # odd = True
+        # mask = []
+        # mask += [False] * self.slope_length
+        # mask += [True] * self.slope_length
+        # repetitions = ((data_size - self.slope_length) / (self.slope_length + self.plateau_length) - 1)
+        # torch.tensor(mask).repeat(repetitions)
+        # while i < data_size:
+        #     if odd:
+        #         mask += [False] * self.slope_length
+        #         i += self.slope_length
+        #         odd = False
+        #     else:
+        #         mask += [True] * self.plateau_length
+        #         i += self.plateau_length
+        #         odd = True
+        # return mask
+        repetitions = int(
+            (
+                (data_size - self.slope_length)
+                / (self.slope_length + self.plateau_length)
+            )
+        )  # -1 ?
+        mask = torch.tensor(self.initial_mask).repeat(repetitions)
+        return torch.cat((mask, self.final_mask))
 
-    def generate_slopped_plateau(self, total_length, value=1):
-        length = total_length - (2 * self.slope_lengths)
-        ramping_up = np.linspace(0, value, self.slope_lengths)
-        ramping_down = np.linspace(value, 0, self.slope_lengths)
-        plato = np.broadcast_to(value, length)
-        return np.concatenate((ramping_up, plato, ramping_down))
+    # def generate_slopped_plateau(self, total_length, value=1):
+    #     length = total_length - (2 * self.slope_length)
+    #     ramping_up = np.linspace(0, value, self.slope_length)
+    #     ramping_down = np.linspace(value, 0, self.slope_length)
+    #     plato = np.broadcast_to(value, length)
+    #     return np.concatenate((ramping_up, plato, ramping_down))
--- test/unit/utils/waveform.py
+++ test/unit/utils/waveform.py
@@ -2,40 +2,54 @@
 Unit tests for the waveform manager
 @author: ualegre
 """
-import unittest2
-import numpy as np
+import unittest2 as unittest
+import torch
 from brainspy.utils.waveform import WaveformManager
 
 
-class WaveformTest(unittest2.TestCase):
-
+class WaveformTest(unittest.TestCase):
     def __init__(self, test_name):
         super(WaveformTest, self).__init__()
         configs = {}
-        configs['plateau_lengths'] = 80
-        configs['slope_lengths'] = 20
+        configs["plateau_lengths"] = 80
+        configs["slope_lengths"] = 20
         self.configs = configs
         self.waveform_mgr = WaveformManager(configs)
 
     def full_check(self, point_no):
-        points = np.random.rand(point_no)
+        points = torch.rand(point_no)
         waveform = self.waveform_mgr.points_to_waveform(points)
-        assert waveform[0] == 0.0 and waveform[-1] == 0.0, 'Waveforms do not start and end with zero'
-        assert len(waveform) == ((self.waveform_mgr.plateau_lengths * len(points)) + (self.waveform_mgr.slope_lengths * (len(points) + 1))), 'Waveform has an incorrect shape'
+        assert (
+            waveform[0] == 0.0 and waveform[-1] == 0.0
+        ), "Waveforms do not start and end with zero"
+        assert len(waveform) == (
+            (self.waveform_mgr.plateau_length * len(points))
+            + (self.waveform_mgr.slope_length * (len(points) + 1))
+        ), "Waveform has an incorrect shape"
 
         mask = self.waveform_mgr.generate_mask(len(waveform))
         assert len(mask) == len(waveform)
 
         waveform_to_points = self.waveform_mgr.waveform_to_points(waveform)
         plateaus_to_points = self.waveform_mgr.plateaus_to_points(waveform[mask])
-        assert (np.float32(points) == np.float32(waveform_to_points)).all() == (np.float32(points) == np.float32(plateaus_to_points)).all(), "Inconsistent to_point conversion"
+        assert (
+            (points.half().float() == waveform_to_points.half().float()).all()
+            == (points.half().float() == plateaus_to_points.half().float()).all()
+            == True
+        ), "Inconsistent to_point conversion"
 
-        points_to_plateau = self.waveform_mgr.points_to_plateau(points)
+        points_to_plateau = self.waveform_mgr.points_to_plateaus(points)
         waveform_to_plateau = self.waveform_mgr.waveform_to_plateaus(waveform)
-        assert (waveform[mask] == points_to_plateau).all() == (waveform[mask] == waveform_to_plateau).all(), "Inconsistent plateau conversion"
+        assert (waveform[mask] == points_to_plateau).all() == (
+            waveform[mask] == waveform_to_plateau
+        ).all(), "Inconsistent plateau conversion"
 
-        plateaus_to_waveform = self.waveform_mgr.plateaus_to_waveform(waveform[mask])
-        assert (waveform == plateaus_to_waveform).all(), "Inconsistent waveform conversion"
+        plateaus_to_waveform, mask = self.waveform_mgr.plateaus_to_waveform(
+            waveform[mask]
+        )
+        assert (
+            waveform == plateaus_to_waveform
+        ).all(), "Inconsistent waveform conversion"
 
     def runTest(self):
         # self.full_check(0)
@@ -44,9 +58,10 @@ class WaveformTest(unittest2.TestCase):
         self.full_check(100)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-    # from matplotlib import pyplot as plt
-    # suite = unittest.TestSuite()
-    # suite.addTest(WaveformTest('test1'))
-    # unittest.TextTestRunner().run(suite)
+    from matplotlib import pyplot as plt
+
+    suite = unittest.TestSuite()
+    suite.addTest(WaveformTest("test1"))
+    unittest.TextTestRunner().run(suite)
