23db751c4965a4a19ac4dbecbe068578d0d8d02f
ualegre <u.alegre@utwente.nl>
2020-11-13 16:00:46 +0100

    Major improvements on the batch normalisation module

--- brainspy/processors/modules/bn.py
+++ brainspy/processors/modules/bn.py
@@ -26,77 +26,112 @@ class DNPU_BatchNorm(nn.Module):
     def __init__(
         self,
         processor,  # It is either a dictionary or the reference to a processor or a DNPU_Base module, or any other of the modules channel, layer, lrf
-        outputs=1,
-        inputs_list=None,
-        current_range=None,
-        current_to_voltage=True,
+        inputs_list=None, # It allows to declare a DNPU_Layer from a configs dictionary
+        input_clip=True, # Whether if the input will be clipped by the 
+        transform_to_voltage=True,
+        input_range=None, # For example: [-1,1], this variable is required if there is an input clip or a transformation to voltage
+        device_output_clip=True,
         batch_norm=True,
+        bn_outputs=1,
         track_running_stats=True
     ):
         # default current_range = 2  * std, where std is assumed to be 1
         super().__init__()
+        self.input_clip = input_clip
+        self.device_output_clip = device_output_clip
+        if input_range is None:
+            assert input_clip is False and transform_to_voltage is False
+
+        self.init_processor(processor, inputs_list)
+        self.init_input_range(input_range)
+        self.init_transform_to_voltage(transform_to_voltage, self.input_range)
+        self.init_batch_norm(batch_norm, bn_outputs, track_running_stats)
+
+
+    def init_processor(self, processor, inputs_list):
         if isinstance(processor, Processor) or isinstance(processor, dict):  # It accepts initialising a processor as a dictionary
             if inputs_list is None:
-                self.base = DNPU(processor)
+                self.processor = DNPU(processor)
             else:
-                self.base = DNPU_Layer(processor, inputs_list)
+                self.processor = DNPU_Layer(processor, inputs_list)
         elif isinstance(processor, DNPU) or isinstance(processor, DNPU_Layer):
-            self.base = processor
+            self.processor = processor
         else:
            assert False, 'The node is not recognised. It needs to be either a model dictionary or an instance of a Processor, a DNPU, or a DNPU_Layer.'
 
+    def init_input_range(self, input_range):
+        self.min_input = input_range[0]
+        self.max_input = input_range[1]
+        self.input_range = torch.ones_like(self.processor.get_input_ranges())
+        self.input_range[:,0] *= self.min_input
+        self.input_range[:,1] *= self.max_input
+
+    def init_batch_norm(self,batch_norm, outputs, track_running_stats):
         if batch_norm:
             self.bn = nn.BatchNorm1d(outputs, affine=False, track_running_stats=track_running_stats).to(device=TorchUtils.get_accelerator_type())
         else:
             self.bn = batch_norm
-        if current_to_voltage:
-            if current_range is None:
-                current_range = torch.ones_like(self.node.get_input_ranges())
-                current_range[:,0] *= - 2
-                current_range[:,1] *= 2
-            self.current_to_voltage = CurrentToVoltage(
-                current_range, self.node.get_input_ranges()
+
+    def init_transform_to_voltage(self, transform_to_voltage, input_range):
+        if transform_to_voltage:
+            self.transform_to_voltage = CurrentToVoltage(
+                input_range, self.processor.get_input_ranges()
             )
         else:
-            self.current_to_voltage = current_to_voltage
+            self.transform_to_voltage = transform_to_voltage
 
-    def forward(self, x):
-        if self.current_to_voltage:
-            x = self.current_to_voltage(x)
-        x = self.base(x)
-        # Cut off values out of the clipping value
-        x = torch.clamp(
-            x,
-            min=self.dnpu.processor.get_clipping_value()[0],
-            max=self.dnpu.processor.get_clipping_value()[1],
-        )
-        # Apply batch normalisation
-        if self.bn:
-            x = self.bn(x)
-        # Apply current to voltage transformation
-        # x = self.current_to_voltage(x)
+    def clamp_input(self,x):
+        if self.input_clip:
+            x = torch.clamp(x,min=self.min_input, max=self.max_input)
         return x
 
+    def transform_input(self,x):
+        if self.transform_to_voltage:
+            x = self.transform_to_voltage(x)
+        return x
+    
+    def clamp_output(self,x): 
+        if self.device_output_clip:
+            x = torch.clamp(x, min=self.processor.get_clipping_value()[0], max=self.processor.get_clipping_value()[1])
+        return x
+
+    def apply_batch_norm(self, x):
+        if self.bn:
+            x = self.bn(x)
+        return x
+
+    def forward(self, x):
+        self.clamped_input = self.clamp_input(x)
+        self.transformed_input = self.transform_input(self.clamped_input)
+        self.dnpu_output = self.processor(self.transformed_input)
+        self.clamped_dnpu_output = self.clamp_output(self.dnpu_output)
+        self.batch_norm_output = self.apply_batch_norm(self.clamped_dnpu_output)
+        
+        return self.batch_norm_output
+
     def regularizer(self):
-        return self.base.regularizer()
+        return self.processor.regularizer()
 
     def hw_eval(self, hw_processor_configs):
-        self.base.hw_eval(hw_processor_configs)
+        self.processor.hw_eval(hw_processor_configs)
 
     def is_hardware(self):
-        return self.base.is_hardware()
+        return self.processor.is_hardware()
 
     def get_clipping_value(self):
-        return self.base.get_clipping_value()
+        return self.processor.get_clipping_value()
 
     def get_control_ranges(self):
-        return self.base.get_control_ranges()
+        return self.processor.get_control_ranges()
 
     def get_control_voltages(self):
-        return self.base.get_control_voltages()
+        return self.processor.get_control_voltages()
 
     def set_control_voltages(self, control_voltages):
-        return self.base.set_control_voltages(control_voltages)
+        return self.processor.set_control_voltages(control_voltages)
+
+    def get_logged_variables(self):
+        return({'a_clamped_input':self.clamped_input,'b_transformed_input':self.transformed_input,'c_dnpu_output':self.dnpu_output,'d_clamped_dnpu_output':self.clamped_dnpu_output,'e_batch_norm_output':self.batch_norm_output})
 
 
 if __name__ == "__main__":
