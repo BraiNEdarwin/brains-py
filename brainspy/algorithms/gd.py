981cc50fa2f1b022007e949f82de31d49a5c1652
ualegre <u.alegre@utwente.nl>
2020-11-24 13:06:06 +0100

    Major improvements to the gd algorithm

--- brainspy/algorithms/gd.py
+++ brainspy/algorithms/gd.py
@@ -7,6 +7,7 @@ from brainspy.utils.pytorch import TorchUtils
 from brainspy.utils.waveform import process_data
 
 
+
 def train(
     model,
     dataloaders,
@@ -17,58 +18,42 @@ def train(
     save_dir=None,
     waveform_transforms=None,
     return_best_model=True,
+    training_data_load_path=None
 ):
-    # with torch.autograd.profiler.profile(use_cuda=not TorchUtils.force_cpu) as prof:
-    train_losses, val_losses = [], []
-    min_val_loss = np.inf
+
+    if training_data_load_path is not None:
+       start_epoch, model, optimizer, train_losses, val_losses, min_val_loss =  load_training(training_data_load_path,model, optimizer)
+    else:
+        start_epoch = 0
+        train_losses, val_losses = [], []
+        min_val_loss = np.inf
+
     looper = trange(configs["epochs"], desc=" Initialising")
+    looper.update(start_epoch)
     model.to(device=TorchUtils.get_accelerator_type())
 
     if 'set_regul_factor' in dir(model) and 'regul_factor' in configs:
         model.set_regul_factor(configs['regul_factor'])
 
     for epoch in looper:
-        running_loss = 0
-        val_loss = 0
-        for inputs, targets in dataloaders[0]:
 
-            inputs, targets = process_data(waveform_transforms, inputs, targets)
-            optimizer.zero_grad()
-            predictions = model(inputs)
-            if logger is not None and "log_ios_train" in dir(logger):
-                logger.log_ios_train(inputs, targets, predictions, model, epoch)
-
-            if "regularizer" in dir(model):
-                loss = criterion(predictions, targets) + model.regularizer()
-            else:
-                loss = criterion(predictions,targets)
-            loss.backward()
-            optimizer.step()
-            running_loss += loss.item()
-
-        train_losses.append(running_loss / len(dataloaders[0]))
+        model, running_loss = default_train_step(model, dataloaders[0], criterion, optimizer, waveform_transforms=waveform_transforms, logger=logger)
+        train_losses.append(running_loss)
         description = "Training Loss: {:.6f}.. ".format(train_losses[-1])
-
+        
         if dataloaders[1] is not None and len(dataloaders[1]) > 0:
-            with torch.no_grad():
-                model.eval()
-                for inputs, targets in dataloaders[1]:
-                    inputs, targets = process_data(waveform_transforms, inputs, targets)
-                    predictions = model(inputs)
-                    if logger is not None and "log_ios_val" in dir(logger):
-                        logger.log_ios_val(inputs, targets, predictions)
-                    val_loss += criterion(predictions, targets).item()
-
-            model.train()
-
-            val_losses.append(val_loss / len(dataloaders[1]))
+            val_loss = default_val_step(model, dataloaders[1],criterion, waveform_transforms=waveform_transforms, logger=logger)
+            val_losses.append(val_loss)
             description += "Test Loss: {:.6f}.. ".format(val_losses[-1])
+            # Save only when peak val performance is reached
             if save_dir is not None and val_losses[-1] < min_val_loss:
                 min_val_loss = val_losses[-1]
                 description += " Saving model ..."
                 torch.save(model, os.path.join(save_dir, "model.pt"))
+                torch.save({'epoch':epoch,'model_state_dict':model.state_dict(),'optimizer_state_dict':optimizer.state_dict(),'train_losses':train_losses,'val_losses':val_losses, 'min_val_loss':min_val_loss},os.path.join(save_dir, "training_data.pkl"))
+                
         looper.set_description(description)
-        if logger is not None and "log_val_predictions" in dir(logger):
+        if logger is not None and "log_performance" in dir(logger):
             logger.log_performance(train_losses, val_losses, epoch)
 
         # TODO: Add a save instruction and a stopping criteria
@@ -90,3 +75,48 @@ def train(
     return model, {
         "performance_history": [torch.tensor(train_losses), torch.tensor(val_losses)]
     }
+
+def load_training(training_data_path, model, optimizer):
+    training_data = torch.load(training_data_path, map_location=TorchUtils.get_accelerator_type())
+    model.load_state_dict(training_data['model_state_dict'])
+    optimizer.load_state_dict(training_data['optimizer_state_dict'])
+    train_losses = training_data['train_losses']
+    val_losses = training_data['val_losses']
+    min_val_loss = training_data['min_val_loss']
+    epoch = training_data['epoch']
+    return epoch, model, optimizer, train_losses, val_losses, min_val_loss
+
+def default_train_step(model, dataloader, criterion, optimizer, waveform_transforms=None, logger=None):
+    train_loss = 0
+    model.train()
+    for inputs, targets in dataloader:
+        inputs, targets = process_data(waveform_transforms, inputs, targets)
+        optimizer.zero_grad()
+        predictions = model(inputs)
+
+
+        if "regularizer" in dir(model):
+            loss = criterion(predictions, targets) + model.regularizer()
+        else:
+            loss = criterion(predictions,targets)
+        loss.backward()
+        optimizer.step()
+        if logger is not None and "log_train_step" in dir(logger):
+            logger.log_train_step(epoch, inputs, targets, predictions, model, loss, running_loss)
+        train_loss += loss.item()
+    train_loss /= len(dataloader)
+    return model, train_loss
+
+def default_val_step(model, dataloader, criterion, waveform_transforms=None, logger=None):
+    with torch.no_grad():
+        val_loss = 0
+        model.eval()
+        for inputs, targets in dataloader:
+            inputs, targets = process_data(waveform_transforms, inputs, targets)
+            predictions = model(inputs)
+            loss = criterion(predictions, targets).item()
+            val_loss += loss
+            if logger is not None and "log_val_step" in dir(logger):
+                logger.log_val_step(epoch, inputs, targets, predictions, model, loss, val_loss)
+        val_loss /= len(dataloader)
+    return val_loss
