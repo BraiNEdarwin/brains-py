9476a5cfe17f8829e6ca4fca5c0e9b80c88787fc
Bozhidar Petrov <b.p.petrov@student.utwente.nl>
2021-02-24 22:52:26 +0100

    documented and tested electrodes.py

--- brainspy/utils/electrodes.py
+++ brainspy/utils/electrodes.py
@@ -2,10 +2,41 @@ import numpy as np
 
 from brainspy.utils.pytorch import TorchUtils
 
-
+# Used in processors/processor.py.
 def merge_electrode_data(
     inputs, control_voltages, input_indices, control_voltage_indices, use_torch=True
 ):
+    """
+    Merge data from two electrodes with the specified indices for each.
+    Need to indicate whether numpy or torch is used. The result will
+    have the same type as the input.
+
+    Example
+    -------
+    Let inputs = [i_1, i_2]  and control_voltages = [c_1, ..., c_5] where i_1, c_1, etc are column vectors.
+    Let input_indices = [0, 2] and control_voltages = [3, 1, 4, 5, 6].
+    Then this method would return [i_1, c_2, i_2, c_1, c_3, c_4, c_5].
+    The result will be a numpy array or a torch tensor depending on the input.
+
+    Parameters
+    ----------
+    inputs: np.array or torch.tensor
+        Data for the input electrodes.
+    control_voltages: np.array or torch.tensor
+        Data for the control electrodes.
+    input_indices: iterable of int
+        Indices of the input electrodes.
+    control_voltage_indices: iterable of int
+        Indices of the control electrodes.
+    use_torch : boolean
+        Indicate whether the data is pytorch tensor (instead of a numpy array)
+
+    Returns
+    -------
+    result: np.array or torch.tensor
+        Array or tensor with merged data.
+
+    """
     result = np.empty(
         (inputs.shape[0], len(input_indices) + len(control_voltage_indices))
     )
@@ -16,19 +47,107 @@ def merge_electrode_data(
     return result
 
 
-def transform_to_voltage(x_val,v_min,v_max,x_min,x_max):
-    w,b = get_map_to_voltage_vars(v_min, v_max, x_min, x_max)
+# Not used anywhere
+def transform_to_voltage(x_val, y_min, y_max, x_min, x_max):
+    """
+    Define a line by two points. Evaluate it at a given point.
+
+    Parameters
+    ----------
+    x_val: float
+        Point at which the line is evaluated.
+    y_min : float
+        Y-coordinate of first point.
+    y_max : float
+        Y-coordinate of second point.
+    x_min : float
+        X-coordinate of first point.
+    x_max : float
+        X-coordinate of second point.
+
+    Returns
+    -------
+    (x_val * w) + b : float
+        The line is defined by w and b and evaluated at x_val.
+    """
+    w, b = get_map_to_voltage_vars(y_min, y_max, x_min, x_max)
     return (x_val * w) + b
 
-def get_map_to_voltage_vars(v_min, v_max, x_min, x_max):
-    return get_scale(v_min,v_max,x_min,x_max), get_offset(v_min,v_max,x_min,x_max)
 
-def get_scale(v_min, v_max, x_min, x_max):
-    v = v_min - v_max
-    x = x_min - x_max
-    return v / x
+# Used in utils/transforms.py and
+def get_map_to_voltage_vars(y_min, y_max, x_min, x_max):
+    """
+    Get the scale and the offset of a line between two points.
 
-def get_offset(v_min, v_max, x_min, x_max):
-    v = (v_max * x_min) - (v_min * x_max)
+    Parameters
+    ----------
+    y_min : float
+        Y-coordinate of first point.
+    y_max : float
+        Y-coordinate of second point.
+    x_min : float
+        X-coordinate of first point.
+    x_max : float
+        X-coordinate of second point.
+
+    Returns
+    -------
+    scale : double
+        Scale of the line.
+    offset: double
+        Offset of the line.
+    """
+    return get_scale(y_min, y_max, x_min, x_max), get_offset(y_min, y_max, x_min, x_max)
+
+
+# Only used in this file.
+def get_scale(y_min, y_max, x_min, x_max):
+    """
+    Find the scale/slope of a line defined by points.
+
+    Parameters
+    ----------
+    y_min : float
+        Y-coordinate of first point.
+    y_max : float
+        Y-coordinate of second point.
+    x_min : float
+        X-coordinate of first point.
+    x_max : float
+        X-coordinate of second point.
+
+    Returns
+    -------
+    (y_min - y_max) / (x_min - x_max) : float
+
+    """
+    y = y_min - y_max
     x = x_min - x_max
-    return v / x
+    return y / x
+
+
+# Only used in this file.
+def get_offset(y_min, y_max, x_min, x_max):
+    """
+    Get the offset/y-intercept of a line defined by two points.
+
+    Parameters
+    ----------
+    y_min : float
+        Y-coordinate of first point.
+    y_max : float
+        Y-coordinate of second point.
+    x_min : float
+        X-coordinate of first point.
+    x_max : float
+        X-coordinate of second point.
+
+    Returns
+    -------
+    ((y_max * x_min) - (y_min * x_max)) / (x_min - x_max) : float
+        The offset of the line.
+
+    """
+    y = (y_max * x_min) - (y_min * x_max)
+    x = x_min - x_max
+    return y / x
---
+++ tests/unit/utils/electrodes-test.py
@@ -0,0 +1,99 @@
+import unittest
+import brainspy.utils.electrodes as electrodes
+import numpy as np
+import torch
+
+
+class ElectrodesTest(unittest.TestCase):
+    def test_merge_numpy(self):
+        # Test merging numpy arrays.
+        inputs = np.array(
+            [
+                [1.0, 5.0, 9.0, 13.0],
+                [2.0, 6.0, 10.0, 14.0],
+                [3.0, 7.0, 11.0, 15.0],
+                [4.0, 8.0, 12.0, 16.0],
+            ]
+        )
+        control_voltages = inputs + np.ones(inputs.shape)
+        input_indices = [0, 2, 4, 6]
+        control_voltage_indices = [7, 5, 3, 1]
+        result = electrodes.merge_electrode_data(
+            inputs=inputs,
+            control_voltages=control_voltages,
+            input_indices=input_indices,
+            control_voltage_indices=control_voltage_indices,
+            use_torch=False,
+        )
+        self.assertEqual(result.shape, (4, 8))
+        self.assertIsInstance(result, np.ndarray)
+        target = np.array(
+            [
+                [1.0, 14.0, 5.0, 10.0, 9.0, 6.0, 13.0, 2.0],
+                [2.0, 15.0, 6.0, 11.0, 10.0, 7.0, 14.0, 3.0],
+                [3.0, 16.0, 7.0, 12.0, 11.0, 8.0, 15.0, 4.0],
+                [4.0, 17.0, 8.0, 13.0, 12.0, 9.0, 16.0, 5.0],
+            ]
+        )
+        for i in range(target.shape[0]):
+            for j in range(target.shape[1]):
+                self.assertEqual(result[i][j], target[i][j])
+
+    def test_merge_torch(self):
+        # Test merging torch tensors.
+        inputs = torch.tensor(
+            [
+                [1.0, 5.0, 9.0, 13.0],
+                [2.0, 6.0, 10.0, 14.0],
+                [3.0, 7.0, 11.0, 15.0],
+                [4.0, 8.0, 12.0, 16.0],
+            ],
+            dtype=torch.float32,
+        )
+        control_voltages = inputs + torch.ones(inputs.shape, dtype=torch.float32)
+        input_indices = [0, 2, 4, 6]
+        control_voltage_indices = [7, 5, 3, 1]
+        result = electrodes.merge_electrode_data(
+            inputs=inputs,
+            control_voltages=control_voltages,
+            input_indices=input_indices,
+            control_voltage_indices=control_voltage_indices,
+            use_torch=True,
+        )
+        self.assertEqual(result.shape, (4, 8))
+        self.assertIsInstance(result, torch.Tensor)
+        target = torch.tensor(
+            [
+                [1.0, 14.0, 5.0, 10.0, 9.0, 6.0, 13.0, 2.0],
+                [2.0, 15.0, 6.0, 11.0, 10.0, 7.0, 14.0, 3.0],
+                [3.0, 16.0, 7.0, 12.0, 11.0, 8.0, 15.0, 4.0],
+                [4.0, 17.0, 8.0, 13.0, 12.0, 9.0, 16.0, 5.0],
+            ],
+            dtype=torch.float32,
+        )
+        for i in range(target.shape[0]):
+            for j in range(target.shape[1]):
+                self.assertEqual(result[i][j], target[i][j])
+
+    def test_line(self):
+        # Test scale and offset, and evaluation at a point, of a line.
+        x_min = 1
+        y_min = 1
+        x_max = 2
+        y_max = 0
+        # This is the line y = 2 - x
+        x_val = 3
+        offset = electrodes.get_offset(
+            y_min=y_min, y_max=y_max, x_min=x_min, x_max=x_max
+        )
+        scale = electrodes.get_scale(y_min=y_min, y_max=y_max, x_min=x_min, x_max=x_max)
+        self.assertEqual(offset, 2)
+        self.assertEqual(scale, -1)
+        value = electrodes.transform_to_voltage(
+            x_val=x_val, y_min=y_min, y_max=y_max, x_min=x_min, x_max=x_max
+        )
+        self.assertEqual(value, -1)
+
+
+if __name__ == "__main__":
+    unittest.main()
