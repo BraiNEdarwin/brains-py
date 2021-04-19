47ffebfacd4d91bb23a76fa5ffadd2f2dcca52d1
ualegre <u.alegre@utwente.nl>
2021-04-19 10:54:45 +0200

    Improvements on the main file to call all tests

--- tests/main.py
+++ tests/main.py
@@ -1,25 +1,9 @@
 import unittest
-import sys
-
-
-class T1(unittest.TestCase):
-    def test_A(self):
-        pass
-
-    def test_B(self):
-        pass
-
-
-class T2(unittest.TestCase):
-    def test_A(self):
-        pass
-
-    def test_B(self):
-        pass
-
 
 if __name__ == "__main__":
     from HtmlTestRunner import HTMLTestRunner
 
-    modules_to_test = unittest.defaultTestLoader.discover(start_dir='tests/', pattern='*.py', top_level_dir=None)
-    HTMLTestRunner(output='/home/unai/Documents/3-programming/brains-py/brains-py/test-reports').run(modules_to_test)
+    modules_to_test = unittest.defaultTestLoader.discover(
+        start_dir="tests/", pattern="*.py", top_level_dir=None
+    )
+    HTMLTestRunner(output="tmp/test-reports").run(modules_to_test)
