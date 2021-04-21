63efcf75fcaa2419dc658989bced8109eabe6eff
ualegre <u.alegre@utwente.nl>
2021-04-21 13:52:11 +0200

    The raw model is now initialised only using model_structure dictionary instead of info dictionary. Renamed this in documentation. No info dictionary is now required inside the NeuralNetworkModel.

--- brainspy/processors/simulation/model.py
+++ brainspy/processors/simulation/model.py
@@ -1,64 +1,76 @@
-"""
-Module for creating and using a neural network model.
-"""
+""" The accelerator class enables to statically access the accelerator
+(CUDA or CPU) that is used in the computer. The aim is to support both platforms seemlessly. """
 import warnings
-from typing import Optional, Dict
 
-import torch
 from torch import nn
+from typing import OrderedDict
+
+
+def info_consistency_check(model_info: OrderedDict) -> OrderedDict:
+    """
+    Check if the model info follows the expected standards:
+    Are input dimension, output dimension, and hidden layer number and sizes
+    are defined in the config? If they aren't, set them to default values and
+    print a warning.
+    Used on the smg_configs specifically.
+
+    Parameters
+    ----------
+    model_info : dict
+        Dictionary of the configs.
+
+    Returns
+    -------
+    model_info : dict
+        A possibly altered version of the input dictionary.
+    """
+    default_in_size = 7
+    default_out_size = 1
+    default_hidden_size = 90
+    default_hidden_number = 6
+    if "D_in" not in model_info:
+        # Check input dimension.
+        model_info["D_in"] = default_in_size
+        warnings.warn(
+            "The model loaded does not define the input dimension as expected. "
+            f"Changed it to default value: {default_in_size}."
+        )
+    if "D_out" not in model_info:
+        # Check output dimension.
+        model_info["D_out"] = default_out_size
+        warnings.warn(
+            "The model loaded does not define the output dimension as expected. "
+            f"Changed it to default value: {default_out_size}."
+        )
+    if "hidden_sizes" not in model_info:
+        # Check sizes of hidden layers.
+        model_info["hidden_sizes"] = default_hidden_size * default_hidden_number
+        warnings.warn(
+            "The model loaded does not define the hidden layer sizes as expected. "
+            f"Changed it to default value: {default_hidden_number} layers of {default_hidden_size}."
+        )
+    return model_info
 
 
 class NeuralNetworkModel(nn.Module):
     """
-    A neural network model is a basic pytorch model.
-
-    Attributes:
-    info : dict
-        Dictionary containing the model info; keys explained in constructor
-        method.
-    verbose : bool
-        Indicate whether to print certain steps.
-    raw_model : nn.Sequential
-        Torch object containing the layers and activations of the network.
+    The TorchModel class is used to manage together a torch model and its state dictionary. The usage is expected to be as follows
+    Loads a neural network from a custom dictionary
+    mymodel = TorchModel()
+    mymodel.load_model('my_path/my_model.pt')
+    mymodel.model
     """
-    def __init__(self, model_info: dict, verbose=False):
-        """
-        Create a model object.
 
-        Parameters
-        ----------
-        model_info : dict
-            Dictionary containing the model info.
-            D_in : int
-                Number of inputs (electrodes).
-            D_out : int
-                Number of outputs (electrodes).
-            activation : str
-                Type of activation,  for example "relu".
-            hidden_sizes : list[int]
-                Sizes of the hidden layers.
-        verbose : bool, optional
-            Whether to print certain steps, by default False.
-        """
+    # TODO: Automatically register the data type according to the configurations of the amplification variable of the  info dictionary
+
+    def __init__(self, verbose=False):
         super(NeuralNetworkModel, self).__init__()
-        self.info: Optional[Dict] = None
+        self.info = None
         self.verbose = verbose
-        self.build_model_structure(model_info)
+        # self.build_model_structure(model_info)
 
-    def build_model_structure(self, model_info: dict):
-        """
-        Build the model from the info dictionary.
-        First perform the consistency check, then set the layers and
-        activations.
-
-        This method is called when an object is created.
-
-        Parameters
-        ----------
-        model_info : dict
-            Dictionary containing the weights and activations of the model.
-        """
-        self.info_consistency_check(model_info)
+    def build_model_structure(self, model_info):
+        model_info = info_consistency_check(model_info)
         hidden_sizes = model_info["hidden_sizes"]
         input_layer = nn.Linear(model_info["D_in"], hidden_sizes[0])
         activ_function = self._get_activation(model_info["activation"])
@@ -76,148 +88,21 @@ class NeuralNetworkModel(nn.Module):
         if self.verbose:
             print("Model built with the following modules: \n", modules)
 
-    def set_info_dict(self, info_dict: dict):
-        """
-        Set the info dictionary of the model.
-
-        Parameters
-        ----------
-        info_dict : dict
-            Info dictionary.
-        """
+    def set_info_dict(self, info_dict):
         self.info = info_dict
 
-    def get_info_dict(self) -> Optional[Dict]:
-        """
-        Get the info dictionary of the model.
-
-        Returns
-        -------
-        dict
-            Info dictionary of the model.
-
-        Raises
-        ------
-        UserWarning
-            If the state dictionary is not set.
-        """
+    def get_info_dict(self):
         if self.info is None:
             warnings.warn("The info dictionary is empty.")
         return self.info
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        """
-        Do a forward pass through the raw neural network model simulating the input-output
-	relationship of a device.
-
-        Example
-        -------
-        >>> model = NeuralNetworkModel(d)
-        >>> model.forward(torch.tensor([1.0, 2.0, 3.0]))
-        torch.Tensor([4.0])
-
-        Parameters
-        ----------
-        x : torch.Tensor
-            Input data.
-
-        Returns
-        -------
-        torch.Tensor
-            Output data.
-        """
+    def forward(self, x):
         return self.raw_model(x)
 
-    def _get_activation(self, activation: str):
-        """
-        Get the activation of the model. If it's a string then return an
-        actual activation object, if that type of activation is implemented.
-        If string is not recognized, raise warning and return relu.
-
-        Example
-        -------
-        >>> model = NeuralNetworkModel(d)
-        >>> model._get_activation("tanh")
-        nn.Tanh
-
-        Parameters
-        ----------
-        activation : str
-            Type of activation, can be relu, elu, tanh, hard-tanh, sigmoid.
-
-        Returns
-        -------
-        activation
-            An activation object.
-
-        Raises
-        ------
-        UserWarning
-            If activation string is not recognized.
-        """
-        if activation == "relu":
+    def _get_activation(self, activation):
+        # TODO: generalize get_activation function to allow for several options, e.g. relu, tanh, hard-tanh, sigmoid
+        if type(activation) is str:
+            if self.verbose:
+                print("Activation function is set as ReLU")
             return nn.ReLU()
-        elif activation == "elu":
-            return nn.ELU()
-        elif activation == "tanh":
-            return nn.Tanh()
-        elif activation == "hard-tanh":
-            return nn.Hardtanh()
-        elif activation == "sigmoid":
-            return nn.Sigmoid()
-        else:
-            warnings.warn("Activation not recognized, applying ReLU")
-            return nn.ReLU()
-        if self.verbose:
-            print(f"Activation function is set as {activation}")
-
-    def info_consistency_check(self, model_info: dict):
-        """
-        Check if the model info follows the expected standards:
-        Are activation, input dimension, output dimension, and hidden layer
-        number and sizes are defined in the config? If they aren't, set them
-        to default values and print a warning.
-
-        This method is called when an object is created.
-
-        Parameters
-        ----------
-        model_info : dict
-            Dictionary of the configs.
-
-        Raises
-        ------
-        UserWarning
-            If a parameter is not in the expected format.
-        """
-        default_in_size = 7
-        default_out_size = 1
-        default_hidden_size = 90
-        default_hidden_number = 6
-        default_activation = "relu"
-        if not ("activation" in model_info):
-            model_info["activation"] = "relu"
-            warnings.warn(
-                "The model loaded does not define the activation as "
-                f"expected. Changed it to default value: {default_activation}."
-            )
-        if "D_in" not in model_info:
-            # Check input dimension.
-            model_info["D_in"] = default_in_size
-            warnings.warn(
-                "The model loaded does not define the input dimension as "
-                f"expected. Changed it to default value: {default_in_size}.")
-        if "D_out" not in model_info:
-            # Check output dimension.
-            model_info["D_out"] = default_out_size
-            warnings.warn(
-                "The model loaded does not define the output dimension as "
-                f"expected. Changed it to default value: {default_out_size}.")
-        if "hidden_sizes" not in model_info:
-            # Check sizes of hidden layers.
-            model_info["hidden_sizes"] = [default_hidden_size
-                                          ] * default_hidden_number
-            warnings.warn(
-                "The model loaded does not define the hidden layer sizes as "
-                f"expected. Changed it to default value: "
-                f"{default_hidden_number} layers of {default_hidden_size}.")
+        return activation
