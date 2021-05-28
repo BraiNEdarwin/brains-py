2c453189056b6568df28139472443a111a58cf33
ualegre <u.alegre@utwente.nl>
2021-05-28 10:02:07 +0200

    Major improvements on the BN module, related to the application of a linear transformation from the data input to voltage.

--- brainspy/processors/modules/bn.py
+++ brainspy/processors/modules/bn.py
@@ -12,7 +12,7 @@ import torch.nn as nn
 
 from brainspy.processors.dnpu import DNPU
 from brainspy.utils.pytorch import TorchUtils
-from brainspy.utils.transforms import CurrentToVoltage
+from brainspy.utils.transforms import get_linear_transform_constants
 
 from brainspy.processors.modules.layer import DNPU_Layer
 from brainspy.processors.processor import Processor
@@ -28,6 +28,7 @@ class DNPU_BatchNorm(nn.Module):
         self,
         processor,  # It is either a dictionary or the reference to a processor or a DNPU_Base module, or any other of the modules channel, layer, lrf
         inputs_list=None,  # It allows to declare a DNPU_Layer from a configs dictionary
+        dnpu_type='for',
         # Transformation configs
         # input_clip=True,  # Whether if the input will be clipped by the
         # transform_to_voltage=True,  # Whether if a transformation to from the data input range to the data input voltages will be applied.
@@ -44,7 +45,7 @@ class DNPU_BatchNorm(nn.Module):
         super(DNPU_BatchNorm, self).__init__()
         # self.input_clip = input_clip
         # self.device_output_clip = device_output_clip
-        self.init_processor(processor, inputs_list)
+        self.init_processor(processor, inputs_list, dnpu_type)
 
         self.input_clip = False
         self.transform_to_voltage = False
@@ -54,33 +55,22 @@ class DNPU_BatchNorm(nn.Module):
         #     self.init_transform_to_voltage(transform_to_voltage, self.input_range)
         # self.init_batch_norm(batch_norm, affine, track_running_stats, momentum)
 
-    def init_processor(self, processor, inputs_list):
-        if isinstance(processor, Processor) or isinstance(
-            processor, dict
-        ):  # It accepts initialising a processor as a dictionary
-            if inputs_list is None:
-                self.processor = DNPU(processor)
-            else:
-                self.processor = DNPU_Layer(processor, inputs_list)
-        elif isinstance(processor, DNPU) or isinstance(processor, DNPU_Layer):
-            self.processor = processor
-        else:
-            assert (
-                False
-            ), "The node is not recognised. It needs to be either a model dictionary or an instance of a Processor, a DNPU, or a DNPU_Layer."
+    def init_processor(self, processor, inputs_list, dnpu_type):
+        self.processor = DNPU(processor, inputs_list, dnpu_type)
 
     def init_input_range(self, input_range):
         self.min_input = input_range[0]
         self.max_input = input_range[1]
-        self.input_range = torch.ones_like(self.processor.get_input_ranges())
-        self.input_range[:, 0] *= self.min_input
-        self.input_range[:, 1] *= self.max_input
+        input_range = torch.ones_like(self.processor.get_input_ranges()).T
+        
+        input_range[0] *= self.min_input
+        input_range[1] *= self.max_input
+        self.input_range = input_range.T
 
     def init_batch_norm(self, affine=False, track_running_stats=True, momentum=0.1, eps=1e-5, custom_bn=None):
         if custom_bn is None:
-            self.init_output_node_no()
             self.bn = nn.BatchNorm1d(
-                self.bn_outputs,
+                self.processor.get_node_no(),
                 affine=affine,
                 track_running_stats=track_running_stats,
                 momentum=momentum,
@@ -89,23 +79,27 @@ class DNPU_BatchNorm(nn.Module):
         else:
             self.bn = custom_bn
 
-    def init_output_node_no(self):
-        if isinstance(self.processor, DNPU):
-            self.bn_outputs = 1  # Number of outputs from the DNPU layer
-        elif isinstance(self.processor, DNPU_Layer):
-            self.bn_outputs = len(self.processor.processor.inputs_list)
-        else:
-            print(
-                "Warning: self.processor in DNPU_BatchNorm is from a type that is not identified. The outputs of batch norm are not automatically detected."
-            )
+    # def init_output_node_no(self):
+    #     if isinstance(self.processor, DNPU):
+    #         self.bn_outputs = 1  # Number of outputs from the DNPU layer
+    #     elif isinstance(self.processor, DNPU_Layer):
+    #         self.bn_outputs = len(self.processor.processor.inputs_list)
+    #     else:
+    #         print(
+    #             "Warning: self.processor in DNPU_BatchNorm is from a type that is not identified. The outputs of batch norm are not automatically detected."
+    #         )
 
     # Strict defines if the input is going to be clipped before doing the linear transformation in order to ensure that the transformation is correct
     def init_transform_to_voltage(self, input_range, strict=True):
         self.input_clip = strict
         self.init_input_range(input_range)
-        self.transform_to_voltage = CurrentToVoltage(
-            self.input_range, self.processor.get_input_ranges(), cut=False
-        )
+        self.transform_to_voltage = True
+        # self.transform_to_voltage = CurrentToVoltage(
+        #     self.input_range, self.processor.get_input_ranges()
+        # )
+        scale, offset = get_linear_transform_constants(self.processor.get_input_ranges().T[0].T, self.processor.get_input_ranges().T[1].T, self.input_range.T[0].T, self.input_range.T[1].T)
+        self.scale = scale.flatten()
+        self.offset = offset.flatten()
 
     def clamp_input(self, x):
         if self.input_clip:
@@ -114,7 +108,7 @@ class DNPU_BatchNorm(nn.Module):
 
     def transform_input(self, x):
         if self.transform_to_voltage:
-            x = self.transform_to_voltage(x)
+            x = (self.scale * x) + self.offset  # self.transform_to_voltage(x)
         return x
 
     def apply_batch_norm(self, x):
