1bbdcfa467e6f28394507d559c2525873e3ebbf2
hnm27 <humaidalimollah73@gmail.com>
2021-08-19 08:54:30 +0200

    added missing the results dictionary description and the configs dictionary description

--- brainspy/algorithms/modules/performance/data.py
+++ brainspy/algorithms/modules/performance/data.py
@@ -1,37 +1,60 @@
 import torch
-from torch.utils.data import SubsetRandomSampler, Dataset, random_split
-
-from brainspy.utils.pytorch import TorchUtils
+from torch.utils.data import Dataset
 
 
 def get_data(results, configs):
     """
     Initialises the perceptron Dataset and loads the dataset into the Pytorch Dataloader.
     The dataloader loads the data into the memory according to the configs dictionary.
-    Refer to https://pytorch.org/tutorials/beginner/basics/data_tutorial.html for DataLoaders in PyTorch
+    Refer to https://pytorch.org/tutorials/beginner/basics/data_tutorial.html for DataLoaders
+    in PyTorch.
 
-    batch size -The batch size defines the number of samples that will be propagated through the network.
-
-    pin_memory - boolean value to pin the data to memory or not.
-                 If you load your samples in the Dataset on CPU and would like to push it during training to the GPU,
-                 you can speed up the host to device transfer by enabling pin_memory.
-
-                 Refer to https://pytorch.org/docs/stable/data.html for batching of data and memory pinning in PyTorch.
-
-    worker_no - number of workers on the pytorch dataset which is by default set to 0.
-                Refer to https://deeplizard.com/learn/video/kWVgvsejXsE to understand how the number of workers are used in deep neural network programming.
-
-
-    The data can be shuffled. After each epoch the data is shuffled automatically. This is by design to accelerate and improve the model training process.
-    Because of this, the learning algorithm is stochastic and may achieve different results each time it is run.
-    Refer to https://pytorch.org/tutorials/beginner/blitz/cifar10_tutorial.html to see how epochs are used when training a Classifier.
+    The data can be shuffled. After each epoch the data is shuffled automatically. This is by
+    design to accelerate and improve the model training process.Because of this, the learning
+    algorithm is stochastic and may achieve different results each time it is run.
+    Refer to https://pytorch.org/tutorials/beginner/blitz/cifar10_tutorial.html to see how epochs
+    are used when training a Classifier.
 
     Parameters
     ----------
     results : torch.Tensor
-        target values required for this perceptron algorithm
+        These contain the input and target values of the perceptron alogorithm.
+        It also contains the normalised input data from which the Pytorch dataloader is created.
+
+        It has the following keys:
+
+        inputs : torch.Tensor
+            The inputs to the perceptron algorithm, which are the outputs of the DNPU or DNPU
+            architectures that you want to evaluate the accuracy against.
+        norm_inputs : torch.Tensor
+            Standard normal distribution of the input data. To calculate this, the
+            zscore_norm function can be used in brainspy.algorithms.modules.performance.accuracy
+        targets : torch.Tensor
+            Binary targets against which the outuut of the perceptron algorithm is compared.
+
     configs : dict
-        configurations of the model
+        Configurations of the model to create the dataloader for the perceptron algorithm.
+        The configs should contain a description of the hyperparameters that can be used
+        to calculate the accuracy of the Perceptron algorithm.
+
+        It has the following keys:
+
+            epochs: int
+                Number of loops used for training the perceptron.
+            learning_rate: float
+                Learning rate used to train the perceptron.
+            data:
+                batch size- The batch size defines the number of samples that will be propagated
+                            through the network.
+                pin_memory- boolean value to pin the data to memory or not.
+                            If you load your samples in the Dataset on CPU and would like to push
+                            it during training to the GPU,you can speed up the host to device
+                            transfer by enabling pin_memory.
+                            Refer to https://pytorch.org/docs/stable/data.html for batching of data
+                            and memory pinning in PyTorch.
+                worker_no - number of workers on the pytorch dataset which is by default set to 0.
+                            Refer to https://deeplizard.com/learn/video/kWVgvsejXsE to understand
+                            how the number of workers are used in deep neural network programming.
 
     Returns
     -------
@@ -39,9 +62,9 @@ def get_data(results, configs):
         Dataloader of the perceptron algorithm
     """
     if configs["data"]["worker_no"] > 0 or configs["data"]["pin_memory"]:
-        dataset = PerceptronDataset(
-            results["norm_inputs"], results["targets"], device=torch.device("cpu")
-        )
+        dataset = PerceptronDataset(results["norm_inputs"],
+                                    results["targets"],
+                                    device=torch.device("cpu"))
     else:
         dataset = PerceptronDataset(results["norm_inputs"], results["targets"])
     dataloaders = torch.utils.data.DataLoader(
@@ -56,13 +79,14 @@ def get_data(results, configs):
 
 class PerceptronDataset(Dataset):
     """
-    This class is an instace of the Pytorch Dataset. It passes all the information onto the Pytorch dataset.
-    Dataset stores the samples and their corresponding labels, and DataLoader wraps an iterable around the Dataset to enable easy access to the samples.
+    This class is an instace of the Pytorch Dataset. It passes all the information onto the Pytorch
+    dataset. The dataset stores the samples and their corresponding labels, and DataLoader wraps an
+    iterable around the Dataset to enable easy access to the samples.
 
-    Refer to https://pytorch.org/tutorials/beginner/basics/data_tutorial.html to see how Pytorch datasets are created and used.
+    Refer to https://pytorch.org/tutorials/beginner/basics/data_tutorial.html to see how Pytorch
+    datasets are created and used.
 
     """
-
     def __init__(self, inputs, targets, device=None):
         """
         Initialize the dataset of the Perceptron
@@ -70,7 +94,8 @@ class PerceptronDataset(Dataset):
         Parameters
         ----------
         inputs : torch.Tensor
-            the inputs to the perceptron algorithm, which are the outputs of the DNPU or DNPU architectures that you want to evaluate the accuracy against
+            the inputs to the perceptron algorithm, which are the outputs of the DNPU or DNPU
+            architectures that you want to evaluate the accuracy against
         targets : torch.Tensor
             binary targets against which the outuut of the perceptron algorithm is compared
         device : torch.Device, optional
@@ -78,14 +103,17 @@ class PerceptronDataset(Dataset):
         """
 
         # Normalise inputs
-        assert len(inputs) > 10, "Not enough data, at least 10 points are required."
+        assert len(
+            inputs) > 10, "Not enough data, at least 10 points are required."
         assert not torch.isnan(inputs).any(), "NaN values detected."
         if device is None:
             self.inputs = inputs.to(dtype=torch.get_default_dtype())
             self.targets = targets.to(dtype=torch.get_default_dtype())
         else:
-            self.inputs = inputs.to(device=device, dtype=torch.get_default_dtype())
-            self.targets = targets.to(device=device, dtype=torch.get_default_dtype())
+            self.inputs = inputs.to(device=device,
+                                    dtype=torch.get_default_dtype())
+            self.targets = targets.to(device=device,
+                                      dtype=torch.get_default_dtype())
 
     def __getitem__(self, index):
         """
