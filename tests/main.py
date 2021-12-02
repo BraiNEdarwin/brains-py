22566fa66428dee490a5680fbd64cbba2777d5a9
ualegre <u.alegre@utwente.nl>
2021-12-02 17:34:40 +0100

    Added timestamp folder for each time a test gets executed

--- tests/main.py
+++ tests/main.py
@@ -7,9 +7,14 @@ import brainspy
 
 if __name__ == "__main__":
     from HtmlTestRunner import HTMLTestRunner
-    brainspy.TEST_MODE = 'SIMULATION_PC'  # Available test modes: SIMULATION_PC, HARDWARE_CDAQ, HARDWARE_NIDAQ 
+    from datetime import datetime
+
+    timestamp = datetime.today().strftime('%d-%m-%Y-%H:%M:%S')
+
+    brainspy.TEST_MODE = 'SIMULATION_PC'  # Available test modes: SIMULATION_PC, HARDWARE_CDAQ, HARDWARE_NIDAQ
     modules_to_test = unittest.defaultTestLoader.discover(start_dir="tests/",
                                                           pattern="*.py",
                                                           top_level_dir=None)
-    HTMLTestRunner(output="tmp/test-reports").run(modules_to_test)
+    HTMLTestRunner(output="tmp/test-reports/" +
+                   str(timestamp)).run(modules_to_test)
     print(brainspy.TEST_MODE)
