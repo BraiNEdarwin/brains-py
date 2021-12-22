3278b3630ae1b61b3efa3b314c820dfdf159be57
ualegre <u.alegre@utwente.nl>
2021-12-22 15:39:01 +0100

    Fixes on the tests to make it work

--- tests/unit/algorithms/modules/performance/data_test.py
+++ tests/unit/algorithms/modules/performance/data_test.py
@@ -8,12 +8,10 @@ from brainspy.algorithms.modules.performance.data import get_data
 
 
 class Data_Test(unittest.TestCase):
-
     """
     Tests for the Perceptron dataloader - data.py.
 
     """
-
     def __init__(self, test_name):
         super(Data_Test, self).__init__()
 
@@ -29,18 +27,13 @@ class Data_Test(unittest.TestCase):
         results["norm_inputs"] = zscore_norm(inputs)
 
         configs = {}
-        configs["data"] = {}
-        configs["data"]["pin_memory"] = True
-        configs["data"]["batch_size"] = 512
-        configs["data"]["worker_no"] = 0
+        configs["batch_size"] = 512
         configs["epochs"] = 130
         configs["learning_rate"] = 0.01
 
         dataloader = get_data(results, configs)
 
-        self.assertEqual(dataloader.pin_memory, True)
         self.assertEqual(dataloader.batch_size, 512)
-        self.assertEqual(dataloader.num_workers, 0)
         self.assertEqual(dataloader.drop_last, False)
 
     def test_get_data1(self):
@@ -55,10 +48,7 @@ class Data_Test(unittest.TestCase):
         results["norm_inputs"] = zscore_norm(inputs)
 
         configs = {}
-        configs["data"] = {}
-        configs["data"]["pin_memory"] = True
-        configs["data"]["batch_size"] = 512
-        configs["data"]["worker_no"] = 0
+        configs["batch_size"] = 512
         configs["epochs"] = 130
         configs["learning_rate"] = 0.01
 
@@ -71,37 +61,35 @@ class Data_Test(unittest.TestCase):
         """
         Test for assertion error if NaN values detected in the input data
         """
-        inputs = torch.tensor(
-            [
-                1,
-                float("nan"),
-                2,
-                1,
-                float("nan"),
-                2,
-                1,
-                float("nan"),
-                2,
-                1,
-                float("nan"),
-                2,
-                1,
-                float("nan"),
-                2,
-                1,
-                float("nan"),
-                2,
-                1,
-                float("nan"),
-                2,
-                1,
-                float("nan"),
-                2,
-                1,
-                float("nan"),
-                2,
-            ]
-        )
+        inputs = torch.tensor([
+            1,
+            float("nan"),
+            2,
+            1,
+            float("nan"),
+            2,
+            1,
+            float("nan"),
+            2,
+            1,
+            float("nan"),
+            2,
+            1,
+            float("nan"),
+            2,
+            1,
+            float("nan"),
+            2,
+            1,
+            float("nan"),
+            2,
+            1,
+            float("nan"),
+            2,
+            1,
+            float("nan"),
+            2,
+        ])
         targets = torch.rand(5)
         results = {}
         results["inputs"] = inputs
@@ -109,10 +97,7 @@ class Data_Test(unittest.TestCase):
         results["norm_inputs"] = zscore_norm(inputs)
 
         configs = {}
-        configs["data"] = {}
-        configs["data"]["pin_memory"] = True
-        configs["data"]["batch_size"] = 512
-        configs["data"]["worker_no"] = 0
+        configs["batch_size"] = 512
         configs["epochs"] = 130
         configs["learning_rate"] = 0.01
 
