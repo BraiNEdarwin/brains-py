77bbb54c32d60f6ba7ba0c5569eba88ee41bf0f7
Srikumar Sastry <s.s.sastry@student.utwente.nl>
2022-05-04 13:51:01 +0200

    MultDNPU

--- tests/unit/processors/dnpu_test.py
+++ tests/unit/processors/dnpu_test.py
@@ -2,7 +2,7 @@ import unittest
 
 import torch
 import numpy as np
-
+import random
 from brainspy.processors import dnpu
 from brainspy.utils.pytorch import TorchUtils
 from brainspy.processors.processor import Processor
@@ -43,6 +43,7 @@ class ProcessorTest(unittest.TestCase):
 
         self.node = Processor(self.configs, self.info)
         self.model = dnpu.DNPU(self.node, data_input_indices=[[3, 4]])
+        self.multi_model = dnpu.DNPU(self.node, data_input_indices=[[3, 4], [3, 4]])
 
     # def test_merge_numpy(self):
     #     """
@@ -98,7 +99,12 @@ class ProcessorTest(unittest.TestCase):
                                       control_data=control_voltages,
                                       input_data_indices=input_indices,
                                       control_indices=control_voltage_indices)
+        result2 = dnpu.merge_electrode_data(input_data=inputs,
+                                      control_data=control_voltages,
+                                      input_data_indices=input_indices,
+                                      control_indices=control_voltage_indices)
         self.assertEqual(result.shape, (4, 8))
+        self.assertEqual(result2.shape, (4, 8))
         self.assertIsInstance(result, torch.Tensor)
         target = torch.tensor(
             [
@@ -112,29 +118,36 @@ class ProcessorTest(unittest.TestCase):
         for i in range(target.shape[0]):
             for j in range(target.shape[1]):
                 self.assertEqual(result[i][j], target[i][j])
+                self.assertEqual(result2[i][j], target[i][j])
 
     def test_forward_pass(self):
         try:
             self.model.set_forward_pass("for")
             self.model.set_forward_pass("vec")
+            self.multi_model.set_forward_pass("for")
+            self.multi_model.set_forward_pass("vec")
         except:
             self.fail("Failed setting forward pass DNPU")
 
         with self.assertRaises(ValueError):
             self.model.set_forward_pass("matrix")
+            self.multi_model.set_forward_pass("matrix")
 
         with self.assertRaises(ValueError):
             self.model.set_forward_pass(["vec"])
+            self.multi_model.set_forward_pass(["vec"])
 
     def test_init_node_no(self):
         try:
             self.model.init_node_no()
+            self.multi_model.init_node_no()
         except:
             self.fail("Failed calculating nodes DNPU")
 
     def test_activ_elec(self):
         try:
             input_data_electrode_no, control_electrode_no = self.model.init_activation_electrode_no()
+            input_data_electrode_no, control_electrode_no = self.multi_model.init_activation_electrode_no()
         except:
             self.fail("Failed Initializing activation electrode DNPU")
 
@@ -147,6 +160,7 @@ class ProcessorTest(unittest.TestCase):
     def test_sample_control(self):
         try:
             control_voltage = self.model.sample_controls()
+            control_voltage = self.multi_model.sample_controls()
         except:
             self.fail("Failed sampling control voltage")
  
@@ -154,14 +168,16 @@ class ProcessorTest(unittest.TestCase):
         try:
             self.model._init_bias()
             self.model._init_learnable_parameters()
+            self.multi_model._init_bias()
+            self.multi_model._init_learnable_parameters()
         except:
             self.fail("Failed Initializing bias and parameters")
 
     def test_forward(self):
         try:
             x = torch.randn(size=(10, 2))
-            #y = self.model.forward(x)
             y = self.model.forward_for(x)
+            y2 = self.multi_model.forward_for(torch.cat((x, x), dim=1))
         except:
             self.fail("Failed Initializing electrode info DNPU")
 
