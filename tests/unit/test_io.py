e2256c664b96c42d21162783c0e1439225810db2
Humaid Mollah <73120385+hnm27@users.noreply.github.com>
2022-08-12 16:12:47 +0200

    test ci

--- tests/unit/test_io.py
+++ tests/unit/test_io.py
@@ -27,7 +27,7 @@ class IOTest(unittest.TestCase):
         configs["batch"] = None
         configs["data"] = {"data1": "New data"}
         self.configs = configs
-        self.path = os.path.join(os.getcwd(), "tests/unit/ut/testfiles")
+        self.path = os.path.join(os.getcwd(), "tests/unit/utils/testfiles")
 
     def test_save_load_configs(self):
         """
